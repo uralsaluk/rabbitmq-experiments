093389cfd380695eb8c31086b75ba534ecb06b1b
Ural Saluk <uralsaluk@gmail.com>
2023-01-04 13:47:36 +0300

    initial commit

---
+++ consumerdemo/src/test/java/com/ural/rabbitmq/consumerdemo/ConsumerdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ural.rabbitmq.consumerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ producerdemo/src/main/java/com/ural/rabbitmq/producerdemo/ProducerdemoApplication.java
@@ -0,0 +1,13 @@
+package com.ural.rabbitmq.producerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducerdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProducerdemoApplication.class, args);
+	}
+
+}
